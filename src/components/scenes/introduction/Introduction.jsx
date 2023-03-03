1957a8e7927864677d25164bfa1cb910cb5f45ec
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2023-03-03 11:23:52 +0100

    feat: init shader movement

--- src/components/scenes/introduction/Introduction.jsx
+++ src/components/scenes/introduction/Introduction.jsx
@@ -1,4 +1,4 @@
-import { Box, Center, Environment, OrbitControls, PerspectiveCamera, Plane } from "@react-three/drei"
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
 import { SceneOne } from "../../objects/scenes/SceneOne"
 import { HotGround } from "../../effects/HotGround"
 
@@ -8,7 +8,7 @@ export const Introduction = () => {
       <PerspectiveCamera makeDefault position={[20, 3, 0]} rotation={[0, Math.PI / 2, 0]} />
       <color attach={"background"} args={["#D0FEEF"]} />
       <fog attach={"fog"} args={["orange",30,60]} />
-      <HotGround />
+      <HotGround scale={5} args={[1,1,50,50]} position={[10, 3, 10]} rotation={[0, Math.PI/2, 0]} />
       <ambientLight intensity={0.7} />
       <SceneOne />
       <OrbitControls />
