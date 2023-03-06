93efc6bca06132a234f1e64e82df2fe31d4c30f7
Edward B <57622377+P4tt4te@users.noreply.github.com>
2023-03-06 17:01:14 +0100

    feat: add tests scenes

--- src/components/scenes/introduction/Introduction.jsx
+++ src/components/scenes/introduction/Introduction.jsx
@@ -1,15 +1,27 @@
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
+import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei"
 import { IntroductionScene } from "./IntroductionScene"
+import { ScreenQuad } from "@react-three/drei"
 import { HotGround } from "../../effects/HotGround"
 
 export const Introduction = () => {
+  //<fog attach={"fog"} args={["orange",30,60]} />
+  /*
+  <ScreenQuad scale={200} rotation={[0,Math.PI/2,0]} position={[-200, 0, -10]}>
+        <meshBasicMaterial />
+  </ScreenQuad>
+  */
+
   return (
     <>
       <PerspectiveCamera makeDefault position={[20, 3, 0]} rotation={[0, Math.PI / 2, 0]} />
       <color attach={"background"} args={["#D0FEEF"]} />
-      <fog attach={"fog"} args={["orange",30,60]} />
-      <HotGround scale={2} args={[30,0.3,500,50]} position={[-30, 1, 0]} rotation={[-Math.PI / 2, Math.PI / 6, Math.PI / 2]} />
-      <ambientLight intensity={0.7} />
+      <HotGround
+        scale={2}
+        args={[30, 0.3, 500, 50]}
+        position={[-30, 1, 0]}
+        rotation={[-Math.PI / 2, Math.PI / 6, Math.PI / 2]}
+      />
+      <ambientLight intensity={1} />
       <IntroductionScene />
       <OrbitControls />
     </>
