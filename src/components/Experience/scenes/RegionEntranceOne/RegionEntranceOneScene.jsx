e1c56f8a9038b27573260518dcd07215135cc1c0
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2023-03-10 15:13:39 +0100

    content: add scene without cam & light

--- src/components/Experience/scenes/RegionEntranceOne/RegionEntranceOneScene.jsx
+++ src/components/Experience/scenes/RegionEntranceOne/RegionEntranceOneScene.jsx
@@ -1,11 +1,15 @@
-import { useGLTF, PerspectiveCamera, Environment } from "@react-three/drei"
+import { useGLTF, PerspectiveCamera, Environment, OrbitControls, Gltf } from "@react-three/drei"
 import { Model } from "./Scene_1"
 
 export const RegionEntranceOneScene = () => {
+  //<Gltf src="/assets/scenes/scene_1.glb" />
+
   return (
     <>
-      <Environment preset="sunset" />
-      <Model position={[12, -15, 0]} />
+      <PerspectiveCamera makeDefault />
+      <ambientLight color={"orange"} />
+      <OrbitControls />
+      <Model position={[8, -15, -12]} rotation={[0, 0, 0]} />
     </>
   )
 }
