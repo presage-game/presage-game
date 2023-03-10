237348263fc3eb1d4f9456652466b077b83397fb
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2023-03-10 12:07:16 +0100

    feat: init scene wip

--- src/components/Experience/scenes/RegionEntranceOne/RegionEntranceOne.jsx
+++ src/components/Experience/scenes/RegionEntranceOne/RegionEntranceOne.jsx
@@ -13,17 +13,13 @@ export const RegionEntranceOne = () => {
 
   return (
     <>
-      <PerspectiveCamera makeDefault position={[20, 3, 0]} rotation={[0, Math.PI / 2, 0]} />
-      <color attach={"background"} args={["#D0FEEF"]} />
       <HotGround
         scale={2}
         args={[30, 0.3, 500, 50]}
         position={[-30, 1, 0]}
         rotation={[-Math.PI / 2, Math.PI / 6, Math.PI / 2]}
       />
-      <ambientLight intensity={1} />
       <RegionEntranceOneScene />
-      <OrbitControls />
     </>
   )
 }
