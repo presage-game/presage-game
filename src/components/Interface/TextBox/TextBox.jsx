d49de4d4cb742e2b4a8d3054f1fffc311af48e0b
victorcngo <hello@victorcango.fr>
2023-03-28 23:36:17 +0200

    chore(TextBox): remove useless rotate

--- src/components/Interface/TextBox/TextBox.jsx
+++ src/components/Interface/TextBox/TextBox.jsx
@@ -112,8 +112,8 @@ export const TextBox = ({
                 .map((word, index) => (
                   <motion.span
                     key={`${textIndex}-${spotIndex}-${index}-${key}`}
-                    initial={{ opacity: 0, y: 20, rotate: 36 }}
-                    animate={{ opacity: 1, y: 0, rotate: 0 }}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.25, delay: index * 0.1 }}
                   >
                     {word}{" "}
@@ -129,8 +129,8 @@ export const TextBox = ({
                 .map((word, index) => (
                   <motion.span
                     key={`${textIndex}-${spotIndex}-${index}-${key}`}
-                    initial={{ opacity: 0, y: 20, rotate: 36 }}
-                    animate={{ opacity: 1, y: 0, rotate: 0 }}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.25, delay: index * 0.1 }}
                   >
                     {word}{" "}
