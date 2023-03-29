4726f433ac8b4d8dcf05e3ad4a6cf98870491afc
Edward B <57622377+P4tt4te@users.noreply.github.com>
2023-03-29 10:28:09 +0200

    fix: remove Adinkra test

--- src/components/Experience/chapterOne/1/Scene.jsx
+++ src/components/Experience/chapterOne/1/Scene.jsx
@@ -7,7 +7,6 @@ export const Scene = ({ cameraRef, switchLerp }) => {
 
   return (
     <>
-      <AdinkraOne switchLerp={switchLerp} />
       <PerspectiveCamera makeDefault ref={cameraRef} />
       <ambientLight color={"orange"} />
       <Model position={[8, -15, -12]} rotation={[0, 0, 0]} />
