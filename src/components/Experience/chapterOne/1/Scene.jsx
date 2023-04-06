62d14ca6dc0804f9d9636113637f502aa4634c76
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2023-04-06 09:26:19 +0200

    feat: init test scene

--- src/components/Experience/chapterOne/1/Scene.jsx
+++ src/components/Experience/chapterOne/1/Scene.jsx
@@ -1,14 +1,17 @@
 import { Model } from "./Model"
 import { CustomCamera } from "../../tools/CustomCamera/CustomCamera"
 import { AdinkraThree } from "../../objects/interactive/AdinkraThree/AdinkraThree"
+import { Gltf } from "@react-three/drei"
 
 export const Scene = ({ switchLerp }) => {
+  //<ambientLight color={"orange"} />
+  //<Model position={[8, -15, -12]} rotation={[0, 0, 0]} />
+
   return (
     <>
       <AdinkraThree switchLerp={switchLerp} />
       <CustomCamera />
-      <ambientLight color={"orange"} />
-      <Model position={[8, -15, -12]} rotation={[0, 0, 0]} />
+      <Gltf position={[8, -15, 100]} src="/assets/scenes/scene_1.2.glb" />
     </>
   )
 }
