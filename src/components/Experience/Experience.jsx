637cbcea34d27dcd78122fa1547d3a0c8efb98ff
Edward B <57622377+P4tt4te@users.noreply.github.com>
2023-04-25 12:23:00 +0200

    feat: init SSAO

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -10,10 +10,19 @@ import { Interface } from "@/components/Interface/Interface"
 import { BlackBars } from "@/components/BlackBars/BlackBars"
 
 import styles from "./Experience.module.scss"
+import { EffectComposer, SSAO } from "@react-three/postprocessing"
+import { useControls } from "leva"
+import { BlendFunction } from "postprocessing"
 
 export const Experience = () => {
   const { scene } = useSelector((state) => state.map)
   const { mapActive } = useSelector((state) => state.ui)
+  const { HighGraphics, blendFunction } = useControls({
+    HighGraphics: {
+      label: "High graphics",
+      value: false,
+    },
+  })
 
   return (
     <div className={styles.root}>
@@ -21,6 +30,11 @@ export const Experience = () => {
       <Interface mapActive={mapActive} />
       <Canvas>
         {mapActive ? <Map /> : scene === 0 ? <RegionEntranceOne /> : <PhosphateMine />}
+        {HighGraphics && (
+          <EffectComposer multisampling={0}>
+            <SSAO samples={20} radius={10} intensity={30} luminanceInfluence={0.4} />
+          </EffectComposer>
+        )}
       </Canvas>
     </div>
   )
