1c5f190f30efe148d179a3440112d39a65f6d38a
victorcngo <hello@victorcango.fr>
2023-04-27 00:45:35 +0200

    test: import scene 2

--- src/components/Experience/chapterOne/2/Scene.jsx
+++ src/components/Experience/chapterOne/2/Scene.jsx
@@ -1,13 +1,20 @@
-import { PerspectiveCamera, Plane } from "@react-three/drei"
+import { Model } from "./Model"
+import { CustomCamera } from "../../tools/CustomCamera/CustomCamera"
+import { Environment } from "@react-three/drei"
+import { WelcomeSign } from "../../objects/interactive/WelcomeSign/WelcomeSign"
 
-export const Scene = () => {
+export const Scene = ({
+  variant,
+  setVariant,
+  pubClicked,
+  setPubClicked,
+  mapClicked,
+  setMapClicked,
+}) => {
   return (
     <>
-      <PerspectiveCamera makeDefault />
-      <ambientLight color={"orange"} />
-      <Plane>
-        <meshBasicMaterial color="red" />
-      </Plane>
+      <Model position={[9, -15, 93]} variant={variant} setVariant={setVariant} />
+      <Environment preset="park" />
     </>
   )
 }
--- src/components/Experience/chapterOne/2/Setup.jsx
+++ src/components/Experience/chapterOne/2/Setup.jsx
@@ -1,9 +1,92 @@
+import { useState, useEffect } from "react"
 import { Scene } from "./Scene"
+import { useDispatch } from "react-redux"
+import { changeOnFocusCamera, changeOnFocusCameraPosition } from "@/store/reducers/userReducer"
+import { CustomCamera } from "../../tools/CustomCamera/CustomCamera"
+import { Environment } from "@react-three/drei"
 
 export const Setup = () => {
+  const [variant, setVariant] = useState("default")
+  const [pubClicked, setPubClicked] = useState(false)
+  const [mapClicked, setMapClicked] = useState(false)
+
+  const dispatch = useDispatch()
+  const changeFocus = (value) => dispatch(changeOnFocusCamera(value))
+  const changeFocusPosition = (value) => dispatch(changeOnFocusCameraPosition(value))
+
+  useEffect(() => {
+    if (pubClicked) {
+      setMapClicked(false)
+      changeFocusPosition({
+        position: {
+          x: -21,
+          y: -2,
+          z: 210,
+        },
+        rotation: {
+          x: Math.PI / 6,
+          y: Math.PI / 6,
+          z: 0,
+        },
+      })
+      changeFocus(true)
+    } else if (mapClicked) {
+      setPubClicked(false)
+      changeFocusPosition({
+        position: {
+          x: 80,
+          y: -3,
+          z: 130,
+        },
+        rotation: {
+          x: 0,
+          y: -Math.PI / 4,
+          z: 0,
+        },
+      })
+      changeFocus(true)
+    } else {
+      changeFocus(false)
+    }
+  }, [pubClicked, mapClicked])
+
+  /*
+  const switchLerp = (value) => {
+    changeFocusPosition({
+      position: {
+        x: -50,
+        y: 0,
+        z: 60,
+      },
+      rotation: {
+        x: -Math.PI / 3,
+        y: 0,
+        z: 0,
+      },
+    })
+    changeFocus(value)
+  }
+  */
+
   return (
     <>
-      <Scene />
+      <Environment preset="forest" />
+      <CustomCamera />
+      <directionalLight
+        intensity={variant === "default" ? 0.9 : 0.5}
+        decay={2}
+        position={[-50, 50, 50]}
+        rotation={[-Math.PI / 2, 0, 0]}
+      />
+      {variant !== "default" && <ambientLight color={"#C65948"} intensity={0.5} />}
+      <Scene
+        variant={variant}
+        setVariant={setVariant}
+        mapClicked={mapClicked}
+        setMapClicked={setMapClicked}
+        pubClicked={pubClicked}
+        setPubClicked={setPubClicked}
+      />
     </>
   )
 }
