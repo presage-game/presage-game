cdf8ae99338240344d4a3b41e94bc85af19fd846
Edward B <57622377+P4tt4te@users.noreply.github.com>
2023-05-09 15:19:34 +0200

    feat: add particules effect

--- src/components/Experience/effects/RainEffect.jsx
+++ src/components/Experience/effects/RainEffect.jsx
@@ -1,20 +1,23 @@
 import { Line } from "@react-three/drei"
 import { useFrame } from "@react-three/fiber"
 import { useEffect, useRef, useState } from "react"
-import { BufferGeometry, Float32BufferAttribute, Vector3 } from "three"
+import { BufferGeometry, Float32BufferAttribute, MathUtils, PointsMaterial, Vector3 } from "three"
 
 export const RainEffect = () => {
   const lineRef = useRef(null)
   const rainGeometry = new BufferGeometry()
   const rainGeoPositions = []
 
-  for(let i =0; i < 100; i++) {
-    rainGeoPositions.push(Math.random * 400 - 200)
-    rainGeoPositions.push(Math.random * 500 - 250)
-    rainGeoPositions.push(Math.random * 400 - 200)
-  }
+  for (let i = 0; i < 100; i++) {
+    const x = Math.random() * 400 - 200
+    const y = Math.random() * 500 - 250
+    const z = Math.random() * 400 - 200
 
-  rainGeometry.setAttribute('position', new Float32BufferAttribute(rainGeoPositions))
+    rainGeoPositions.push(x, y, z)
+  }
+  console.log(rainGeoPositions)
+
+  rainGeometry.setAttribute("position", new Float32BufferAttribute(rainGeoPositions, 3))
 
   const config = {
     y: {
@@ -32,7 +35,7 @@ export const RainEffect = () => {
       [
         [-20, 8, -90],
         [-20, 9, -90],
-      ]
+      ],
     ],
   }
 
@@ -50,9 +53,10 @@ export const RainEffect = () => {
   return (
     <>
       <group ref={lineRef}>
-        <points geometry={new BufferGeometry(new )}>
-            <pointsMaterials transparent size={0.1} />
-        </points>
+        <points
+          material={new PointsMaterial({ transparent: true, size: 10, alphaTest: 0.5, color: 0xaaaaaa })}
+          geometry={rainGeometry}
+        ></points>
       </group>
     </>
   )
