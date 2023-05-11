54b6182203c4c0c866fc9d68ee965a79a80fb500
victorcngo <hello@victorcango.fr>
2023-05-11 23:38:35 +0200

    feat(AudioManager): update

--- src/components/AudioManager/AudioManager.jsx
+++ src/components/AudioManager/AudioManager.jsx
@@ -1,4 +1,7 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
+import { useDispatch, useSelector } from "react-redux"
+
+import { intersectScene, intersectPinpoint } from "@/store/reducers/mapReducer"
 
 // Context
 const audioCtx = new AudioContext()
@@ -7,10 +10,6 @@ const audioCtx = new AudioContext()
 const trackGainNode = audioCtx.createGain()
 const ambianceGainNode = audioCtx.createGain()
 const mapGainNode = audioCtx.createGain()
-
-trackGainNode.connect(audioCtx.destination)
-ambianceGainNode.connect(audioCtx.destination)
-mapGainNode.connect(audioCtx.destination)
 
 // Audios
 const trackAudio = new Audio()
@@ -26,77 +25,56 @@ trackSourceNode.connect(trackGainNode)
 ambianceSourceNode.connect(ambianceGainNode)
 mapSourceNode.connect(mapGainNode)
 
+trackGainNode.connect(audioCtx.destination)
+ambianceGainNode.connect(audioCtx.destination)
+mapGainNode.connect(audioCtx.destination)
+
 export const AudioManager = ({ sceneIndex, pinpointIndex, mapActive }) => {
+  const { isSceneIntersecting, isPinpointIntersecting } = useSelector((state) => state.map)
+
+  const dispatch = useDispatch()
+
   useEffect(() => {
-    // Approaching a scene on map
-    if (sceneIndex !== null && mapActive) {
+    if (isSceneIntersecting) {
       ambianceAudio.src = `src/assets/audios/chapterOne/scenes/scene-${sceneIndex}-ambiance.mp3`
-
-      ambianceAudio.addEventListener("loadedmetadata", () => {
-        ambianceAudio.play()
-        ambianceGainNode.gain.setValueAtTime(0, audioCtx.currentTime)
-        ambianceGainNode.gain.linearRampToValueAtTime(0.25, audioCtx.currentTime + 2)
-      })
+      ambianceAudio.play()
+      ambianceGainNode.gain.linearRampToValueAtTime(0.2, audioCtx.currentTime + 2)
+    } else {
+      ambianceGainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 2)
     }
+  }, [isSceneIntersecting])
 
-    // Entering a scene
-    if (sceneIndex !== null && !mapActive) {
+  useEffect(() => {
+    if (isPinpointIntersecting) {
+      ambianceAudio.src = `src/assets/audios/chapterOne/pinpoints/pinpoint-${pinpointIndex}-ambiance.mp3`
+      ambianceAudio.play()
+      ambianceGainNode.gain.linearRampToValueAtTime(0.2, audioCtx.currentTime + 2)
+    } else {
+      ambianceGainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 2)
+    }
+  }, [isPinpointIntersecting])
+
+  useEffect(() => {
+    // If we enter the scene
+    if (sceneIndex !== null && !isSceneIntersecting) {
       trackAudio.src = `src/assets/audios/chapterOne/scenes/scene-${sceneIndex}-track.mp3`
-
-      trackAudio.addEventListener("loadedmetadata", () => {
-        trackAudio.play()
-        trackGainNode.gain.setValueAtTime(0, audioCtx.currentTime)
-        trackGainNode.gain.linearRampToValueAtTime(1, audioCtx.currentTime + 2)
-      })
-
-      mapGainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 2)
-      // setTimeout(() => {
-      //   mapAudio.pause()
-      // }, 2000)
+      trackAudio.play()
+      trackGainNode.gain.linearRampToValueAtTime(1.0, audioCtx.currentTime + 2)
     }
+  }, [sceneIndex])
 
-    // Going away from a scene on map
-    if (sceneIndex === null) {
+  useEffect(() => {
+    if (mapActive) {
       trackGainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 2)
       ambianceGainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 2)
-      // setTimeout(() => {
-      //   trackAudio.pause()
-      //   ambianceAudio.pause()
-      // }, 2000)
-    }
-  }, [sceneIndex, mapActive])
 
-  useEffect(() => {
-    // Approaching a pinpoint on map
-    if (pinpointIndex !== null) {
-      ambianceAudio.src = `src/assets/audios/chapterOne/pinpoints/pinpoint-${pinpointIndex}-ambiance.mp3`
-
-      ambianceAudio.addEventListener("loadedmetadata", () => {
-        ambianceAudio.play()
-        ambianceGainNode.gain.setValueAtTime(0, audioCtx.currentTime)
-        ambianceGainNode.gain.linearRampToValueAtTime(0.25, audioCtx.currentTime + 2)
-      })
-    }
-
-    // Going away from a pinpoint on map
-    if (pinpointIndex === null) {
-      ambianceGainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 2)
-      // setTimeout(() => {
-      //   ambianceAudio.pause()
-      // }, 2000)
-    }
-  }, [pinpointIndex])
-
-  useEffect(() => {
-    // Leaving a scene or opening map
-    if (mapActive) {
-      mapAudio.src = "src/assets/audios/map/map-track.mp3"
-
-      mapAudio.addEventListener("loadedmetadata", () => {
-        mapAudio.play()
-        mapGainNode.gain.setValueAtTime(0, audioCtx.currentTime)
-        mapGainNode.gain.linearRampToValueAtTime(1, audioCtx.currentTime + 2)
-      })
+      mapAudio.src = `src/assets/audios/map/map-track.mp3`
+      mapAudio.play()
+      mapGainNode.gain.linearRampToValueAtTime(0.75, audioCtx.currentTime + 2)
+    } else {
+      dispatch(intersectScene(false))
+      console.log(isSceneIntersecting)
+      mapGainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 2)
     }
   }, [mapActive])
 }
