8a5589a2eb4b59c602be90a844d522d018fcb0d2
Edward B <57622377+P4tt4te@users.noreply.github.com>
2023-05-11 12:08:19 +0200

    fix: hover button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,19 +1,23 @@
 import { useDispatch } from "react-redux"
 import "./Button.scss"
 import { changeMouseVariant } from "@/store/reducers/uiReducer"
+import { useEffect } from "react"
 
 export const Button = ({ text, onClick, variant }) => {
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    return () => {
+      dispatch(changeMouseVariant("default"))
+    }
+  }, [])
 
   return (
     <button
       onPointerEnter={() => dispatch(changeMouseVariant("buttonHover"))}
       onPointerLeave={() => dispatch(changeMouseVariant("default"))}
       className={`${variant === "main" && "Button--main"} Button`}
-      onClick={() => {
-        dispatch(changeMouseVariant("default"))
-        onClick()
-      }}
+      onClick={() => onClick()}
     >
       <div className="Button__border">{text}</div>
     </button>
