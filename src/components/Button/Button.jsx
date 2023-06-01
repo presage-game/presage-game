361ff48cf6fd8145a682ae88a37dc2cf0d73b847
victorcngo <hello@victorcango.fr>
2023-06-01 16:47:37 +0200

    chore: tweak styles

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,21 +1,8 @@
-import { useDispatch } from "react-redux"
 import "./Button.scss"
-import { changeMouseVariant } from "@/store/reducers/uiReducer"
-import { useEffect } from "react"
 
 export const Button = ({ text, onClick, variant }) => {
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    return () => {
-      dispatch(changeMouseVariant("default"))
-    }
-  }, [])
-
   return (
     <button
-      onPointerEnter={() => dispatch(changeMouseVariant("buttonHover"))}
-      onPointerLeave={() => dispatch(changeMouseVariant("default"))}
       className={`${variant === "main" ? "Button--main" : ""} Button`}
       onClick={() => onClick()}
     >
