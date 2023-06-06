f6259dd4e86eb4e5f2dfeb7eaf4468971b867784
victorcngo <hello@victorcango.fr>
2023-06-06 14:07:46 +0200

    feat: collect adinkras anim

--- src/components/Interface/Collection/Collection.jsx
+++ src/components/Interface/Collection/Collection.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { useSelector } from "react-redux"
 import "./Collection.scss"
 
@@ -6,14 +7,14 @@ import adinkraTwoIcon from "@/assets/img/adinkra-2.svg"
 import adinkraThreeIcon from "@/assets/img/adinkra-3.svg"
 
 export const Collection = () => {
-  const { collectedAdinkras } = useSelector((state) => state.user)
+  const { adinkras } = useSelector((state) => state.user)
   const adinkrasIcons = [adinkraOneIcon, adinkraTwoIcon, adinkraThreeIcon]
 
   return (
     <div className="Collection">
       {adinkrasIcons.map((icon, index) => {
         return (
-          <div key={index} className={`item ${collectedAdinkras[index] ? "item--active" : ""}`}>
+          <div key={index} className={`item ${adinkras[index].isCollected ? "item--active" : ""}`}>
             <div className="item__icon">
               <img src={icon} />
             </div>
